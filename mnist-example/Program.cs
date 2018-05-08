572b93e112e2a0818ed26378db213162f2d1972a
Marvin <marvin.zwolsman@gmail.com>
2018-05-08 17:17:22 +0200

    Loading training image count and training labels

--- mnist-example/Program.cs
+++ mnist-example/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace mnist_example
 {
@@ -7,6 +9,25 @@ namespace mnist_example
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var trainLabels = File.ReadAllBytes("data/train-labels.idx1-ubyte").Skip(sizeof(int) * 2).ToArray();
+            var trainImages = File.ReadAllBytes("data/train-images.idx3-ubyte").Skip(sizeof(int)).ToArray();
+
+
+            var totalImages = ReadInt(trainImages);
+            var rows = ReadInt(trainImages, sizeof(int));
+            var cols = ReadInt(trainImages, sizeof(int) * 2);
+
+            if (totalImages != trainLabels.Length)
+                throw new FormatException("The training data doesn't match the training labels");
+            Console.WriteLine($"Number of images: {totalImages}");
+        }
+
+        private static int ReadInt(byte[] input, int offset = 0) {
+
+            var raw = input.Skip(offset).Take(sizeof(int));
+            if (BitConverter.IsLittleEndian)
+                raw = raw.Reverse();
+            return BitConverter.ToInt32(raw.ToArray(), 0);
         }
     }
 }
