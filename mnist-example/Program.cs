6bf0938877cbe279ca465bba8b8d296a41a49ff6
Marvin <marvin.zwolsman@gmail.com>
2018-05-08 17:40:54 +0200

    Basic learning of the numbers

--- mnist-example/Program.cs
+++ mnist-example/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using toynncore;
 
 namespace mnist_example
 {
     class Program
     {
+
+        const int Cycli = 50;
+
+        static Random _random = new Random();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -20,6 +26,44 @@ namespace mnist_example
             if (totalImages != trainLabels.Length)
                 throw new FormatException("The training data doesn't match the training labels");
             Console.WriteLine($"Number of images: {totalImages}");
+
+            var trainingData = new Tuple<double[], double[]>[totalImages];
+
+            var inputs = rows * cols;
+
+            for (int i = 0; i < totalImages; i++) {
+                var label = trainLabels[i];
+                var data = trainImages.Skip(inputs * i).Take(inputs).Select(b => b / 255.0).ToArray();
+                trainingData[i] = new Tuple<double[], double[]>(CreateTargets(label), data);
+            }
+
+
+            var tindex = _random.Next(totalImages);
+            var target = trainLabels[tindex];
+            Console.WriteLine("Target: " + target);
+            //Cleanup?
+            trainLabels = null;
+            trainImages = null;
+
+            var nn = new NeuralNetwork(inputs, 128, 32, 16, 10);
+            while(true) {
+                for (int i = 0; i < Cycli; i++) {
+                    var data = trainingData[_random.Next(totalImages)];
+                    nn.Train(data.Item2, data.Item1);
+                }
+
+                var result = nn.Predict(trainingData[tindex].Item2).ToList();
+                var digit = result.IndexOf(result.Max());
+                Console.WriteLine(digit);
+                Console.CursorLeft = 0;
+                Console.CursorTop -= 1;
+            }
+        }
+
+        private static double[] CreateTargets(int target) {
+            var targets = new double[10];
+            targets[target] = 1.0;
+            return targets;
         }
 
         private static int ReadInt(byte[] input, int offset = 0) {
